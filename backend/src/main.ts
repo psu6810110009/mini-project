d6f700ac232ef3be90ce030b983d74c36ff20a41
psu6810110198 <6810110198@psu.ac.th>
2025-12-30 15:39:02 +0700

    update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // 1. เปิดให้คนอื่นเข้าถึงได้ (CORS)
+  app.enableCors(); 
+
+  // 2. ใช้ Port ที่ Server ให้มา หรือถ้าไม่มีให้ใช้ 3000
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
